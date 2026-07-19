8b7178a7962fafc8268b2e549b72ea2594bb42b5
Zax <sneakjunkmail@gmail.com>
2026-07-19 01:16:04 -0400

    fixed _redirect again

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ connectDB();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://scrollcity.zerric.com',
   credentials: true
 }));
 app.use(morgan('dev'));
